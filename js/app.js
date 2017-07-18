e7d3fba63b1db6684963f0233c492e21acbc96f8
Waiting7777 <waiting0819@msn.com>
2017-07-18 16:58:51 +0800

    init project

---
+++ js/app.js
@@ -0,0 +1,11 @@
+var BjsApp = BjsApp || {}
+
+BjsApp.init = function(){
+
+    var canvas = document.getElementById('renderCanvas')
+
+    var engine = new BABYLON.Engine(canvas, true)
+
+    var scene = new BABYLON.Scene(engine)
+
+}
