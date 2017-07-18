42ed2d09720b62a349b5462dd62315fd0c0da299
Waiting7777 <waiting0819@msn.com>
2017-07-18 17:04:34 +0800

    Add a sphere

--- js/app.js
+++ js/app.js
@@ -8,4 +8,15 @@ BjsApp.init = function(){
 
     var scene = new BABYLON.Scene(engine)
 
+    var camera = new BABYLON.FreeCamera('FreeCamera', new BABYLON.Vector3(0, 2, -15), scene)
+
+    var light = new BABYLON.HemisphericLight('light1', new BABYLON.Vector3(0, 1, 0), scene)
+
+    var sphere = BABYLON.Mesh.CreateSphere('sphere1', 16, 2, scene)
+    sphere.position.y = 1
+
+    engine.runRenderLoop(function(){
+        scene.render()
+    })
+
 }
