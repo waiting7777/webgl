b989a2c99ea956944432dfce8464226b3164ac2a
Waiting7777 <waiting0819@msn.com>
2017-07-19 18:43:13 +0800

    add planet

--- js/solar.js
+++ js/solar.js
@@ -12,8 +12,21 @@ BjsApp.init = function(){
     camera.attachControl(canvas)
 
     var light = new BABYLON.HemisphericLight('light1', new BABYLON.Vector3(0, 1, 0), scene)
+    light.intensity = 0.5
+    light.groundColor = new BABYLON.Color3(0, 0, 1)
+
+    scene.clearColor = new BABYLON.Color3(0, 0, 0)
 
     var sun = BABYLON.Mesh.CreateSphere('sun', 16, 4, scene)
+
+    var planetMaterial = new BABYLON.StandardMaterial('planetMat', scene)
+    planetMaterial.diffuseTexture = new BABYLON.Texture('./assets/images/sand.jpg', scene)
+    planetMaterial.specularColor = new BABYLON.Color3(0, 0, 0)
+
+    var planet1 = BABYLON.Mesh.CreateSphere('planet1', 16, 1, scene)
+    planet1.position.x = 4
+    planet1.material = planetMaterial
+    
 
     engine.runRenderLoop(function(){
         scene.render()
